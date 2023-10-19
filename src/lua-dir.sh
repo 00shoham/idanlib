bbd0632fdf9aa350c9553d56ecae223b66badc9c
Idan Shoham <idan@shoham.ca>
2023-10-19 10:09:01 -0600

    More cross-platform-compatible lua-locator.

---
+++ src/lua-dir.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+LUADIR=`find /usr/include -type d -name '*lua*' | sort -rn | head -1`
+if [ -z "$LUADIR" ] ; then
+  if [ -f "/usr/include/lua.h" ]; then
+    echo "No special include directive required" > /dev/null
+    exit 0
+  else
+    echo "-ICannot-locate-lua.h"
+    exit 1
+  fi
+fi
+
+if [ -f "$LUADIR/lua.h" ] ; then
+  echo "-I$LUADIR"
+  exit 0
+else
+  echo "-Ino-lua.h-in-$LUADIR"
+  exit 1
+fi
