9b017703b7c8dcefa133991f1f317e4e5b6d59c6
Idan Shoham <00shoham@gmail.com>
2023-11-22 15:09:50 -0700

    Include tiny-aes.h

--- src/utils.h
+++ src/utils.h
@@ -65,6 +65,7 @@ extern pthread_mutex_t luaLock;
 #include "nargv.h"
 #include "globs.h"
 #include "sha-wrapper.h"
+#include "tiny-aes.h"
 #include "aes-wrapper.h"
 #include "curl-wrapper.h"
 #include "lua-wrapper.h"
